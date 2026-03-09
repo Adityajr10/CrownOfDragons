c5e8b2a72cc0c951ad0134c016e27c9becdf5c71
Adityajr10 <brodyrai16@gmail.com>
2026-03-09 23:50:52 +0530

    Dragon Ability Data

---
+++ Source/FlightSystem/Public/AI/Data/DragonAbilityData.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Data/DragonAITypes.h"
+#include "DragonAbilityData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FDragonAbilityData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	EDragonAbilityType AbilityType = EDragonAbilityType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	float Cooldown = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	float EnergyCost = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	float PreferredDistanceMin = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	float PreferredDistanceMax = 5000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ability")
+	float Weight = 1.f;
+};
