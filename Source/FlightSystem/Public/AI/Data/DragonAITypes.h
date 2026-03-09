365e8809c79c10ad4ce6d2e64b41a7caba67e000
Adityajr10 <brodyrai16@gmail.com>
2026-03-09 23:49:03 +0530

    New Architecture for AI here DragonAiBehaviourComponent will act as brain for ai to decide its each behaviour
    Creating base ai behaviour component and Enum file for its state

---
+++ Source/FlightSystem/Public/AI/Data/DragonAITypes.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DragonAITypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EDragonState : uint8
+{
+	Idle        UMETA(DisplayName="Idle"),
+	Roaming     UMETA(DisplayName="Roaming"),
+	Observe     UMETA(DisplayName="Observe"),
+	Attack      UMETA(DisplayName="Attack"),
+	Rest        UMETA(DisplayName="Rest"),
+	Flee        UMETA(DisplayName="Flee"),
+	Perch       UMETA(DisplayName="Perch")
+};
+
+UENUM(BlueprintType)
+enum class EDragonAbilityType : uint8
+{
+	None            UMETA(DisplayName="None"),
+	DiveBomb        UMETA(DisplayName="DiveBomb"),
+	AirStrafe       UMETA(DisplayName="AirStrafe"),
+	GroundStrafe    UMETA(DisplayName="GroundStrafe"),
+	FireBreath      UMETA(DisplayName="FireBreath"),
+	FireRain        UMETA(DisplayName="FireRain"),
+	ClawAttack      UMETA(DisplayName="ClawAttack"),
+	WingGust        UMETA(DisplayName="WingGust")
+};
