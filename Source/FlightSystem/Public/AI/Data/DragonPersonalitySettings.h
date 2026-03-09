54bac03935eebe28c295005ff6e1b0ad39b265f3
Adityajr10 <brodyrai16@gmail.com>
2026-03-09 23:50:13 +0530

    Struct for behaviour Comnponent
    Adding Personality struct for component

---
+++ Source/FlightSystem/Public/AI/Data/DragonPersonalitySettings.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DragonPersonalitySettings.generated.h"
+
+USTRUCT(BlueprintType)
+struct FDragonPersonalitySettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Aggression = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Curiosity = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Courage = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Territoriality = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Patience = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Personality")
+	float Intelligence = 0.5f;
+};
