c9de30e580293dc9b8a705dfc7ecb5ecbd0ef40b
Adityajr10 <brodyrai16@gmail.com>
2026-03-17 22:09:03 +0530

    Adding ShadowTarget

---
+++ Source/FlightSystem/Private/AI/Task/BTTask_ShadowTarget.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_ShadowTarget.h"
+#include "AIController.h"
+#include "AI/Component/DragonAbilityComponent.h"
+#include "AI/Ability/DragonAbility.h"
+#include "Kismet/GameplayStatics.h"
+
+UBTTask_ShadowTarget::UBTTask_ShadowTarget()
+{
+	NodeName = "Shadow Target";
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_ShadowTarget::ExecuteTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return EBTNodeResult::Failed;
+
+	ADragonBaseAI* Dragon =
+		Cast<ADragonBaseAI>(AI->GetPawn());
+
+	if (!Dragon) return EBTNodeResult::Failed;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+
+	if (!AbilityComp || !AbilityClass)
+		return EBTNodeResult::Failed;
+
+	AActor* Player =
+		UGameplayStatics::GetPlayerPawn(Dragon, 0);
+
+	UDragonAbility* Ability =
+		NewObject<UDragonAbility>(Dragon, AbilityClass);
+
+	AbilityComp->StartAbility(Ability, Player);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_ShadowTarget::TickTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory,
+	float DeltaSeconds)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return;
+
+	ADragonBaseAI* Dragon =
+		Cast<ADragonBaseAI>(AI->GetPawn());
+
+	if (!Dragon) return;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+
+	if (!AbilityComp->IsAbilityActive())
+	{
+		FinishLatentTask(
+			OwnerComp,
+			EBTNodeResult::Succeeded
+		);
+	}
+}
---
+++ Source/FlightSystem/Public/AI/Task/BTTask_ShadowTarget.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_ShadowTarget.generated.h"
+
+
+class UDragonAbility;
+UCLASS()
+class FLIGHTSYSTEM_API UBTTask_ShadowTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+
+	UBTTask_ShadowTarget();
+
+	virtual EBTNodeResult::Type ExecuteTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void TickTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category="Ability")
+	TSubclassOf<UDragonAbility> AbilityClass;
+};
