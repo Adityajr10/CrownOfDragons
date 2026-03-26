a44a262033e019c917cb0a7d3f3917d523d4db36
Adityajr10 <brodyrai16@gmail.com>
2026-03-26 22:53:47 +0530

    Adding Search area task

---
+++ Source/FlightSystem/Private/AI/Task/BTTask_SearchArea.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_SearchArea.h"
+#include "AIController.h"
+#include "AI/Component/DragonAbilityComponent.h"
+#include "AI/Ability/DragonAbility_SearchArea.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_SearchArea::UBTTask_SearchArea()
+{
+	NodeName = "Search Area";
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_SearchArea::ExecuteTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return EBTNodeResult::Failed;
+
+	ADragonBaseAI* Dragon =
+		Cast<ADragonBaseAI>(AI->GetPawn());
+
+	if (!Dragon) return EBTNodeResult::Failed;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+
+	if (!AbilityComp || !AbilityClass)
+		return EBTNodeResult::Failed;
+
+	FVector SearchLocation =
+		OwnerComp.GetBlackboardComponent()
+		->GetValueAsVector(SearchLocationKey.SelectedKeyName);
+
+	UDragonAbility_SearchArea* Ability =
+		NewObject<UDragonAbility_SearchArea>(Dragon, AbilityClass);
+
+	Ability->SetSearchLocation(SearchLocation);
+
+	AbilityComp->StartAbility(Ability, nullptr);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_SearchArea::TickTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory,
+	float DeltaSeconds)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return;
+
+	ADragonBaseAI* Dragon =
+		Cast<ADragonBaseAI>(AI->GetPawn());
+
+	if (!Dragon) return;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+
+	if (!AbilityComp->IsAbilityActive())
+	{
+		FinishLatentTask(
+			OwnerComp,
+			EBTNodeResult::Succeeded);
+	}
+}
---
+++ Source/FlightSystem/Public/AI/Task/BTTask_SearchArea.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_SearchArea.generated.h"
+
+class UDragonAbility;
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTSYSTEM_API UBTTask_SearchArea : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+
+	UBTTask_SearchArea();
+
+	virtual EBTNodeResult::Type ExecuteTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void TickTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category="Ability")
+	TSubclassOf<UDragonAbility> AbilityClass;
+
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector SearchLocationKey;
+};
