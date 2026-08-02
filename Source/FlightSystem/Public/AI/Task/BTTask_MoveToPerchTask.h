93e8968a98521189c10b9de34efe52976f4eba7b
Adityajr10 <brodyrai16@gmail.com>
2026-08-02 21:58:37 +0530

    MoveToPerch task

---
+++ Source/FlightSystem/Public/AI/Task/BTTask_MoveToPerchTask.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_MoveToPerchTask.generated.h"
+
+class UDragonAbility;
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTSYSTEM_API UBTTask_MoveToPerchTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+
+	UBTTask_MoveToPerchTask();
+
+	virtual EBTNodeResult::Type ExecuteTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void TickTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category="Ability")
+	TSubclassOf<UDragonAbility> AbilityClass;
+
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector PerchLocationKey;
+};
