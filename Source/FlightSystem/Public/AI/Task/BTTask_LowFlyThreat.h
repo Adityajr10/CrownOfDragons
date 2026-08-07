2c54e270ea1e470a482095fb88201e01bea0d960
Adityajr10 <brodyrai16@gmail.com>
2026-08-07 19:58:16 +0530

    Bttask_LowFlyThreat

---
+++ Source/FlightSystem/Public/AI/Task/BTTask_LowFlyThreat.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_LowFlyThreat.generated.h"
+
+class UDragonAbility;
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTSYSTEM_API UBTTask_LowFlyThreat : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+
+	UBTTask_LowFlyThreat();
+
+	virtual EBTNodeResult::Type ExecuteTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void TickTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds) override;
+
+protected:
+
+	/* Ability class to execute */
+	UPROPERTY(EditAnywhere, Category="Ability")
+	TSubclassOf<UDragonAbility> AbilityClass;
+};
