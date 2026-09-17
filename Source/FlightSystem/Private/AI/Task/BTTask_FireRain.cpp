96ca26de75f86896ac961c4178ab3e787ea24555
Adityajr10 <brodyrai16@gmail.com>
2026-09-17 16:18:00 +0530

    Firerain header and cpp

---
+++ Source/FlightSystem/Private/AI/Task/BTTask_FireRain.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_FireRain.h"
+#include "AIController.h"
+#include "AI/Component/DragonAbilityComponent.h"
+#include "AI/Ability/DragonAbility.h"
+#include "Kismet/GameplayStatics.h"
+
+UBTTask_FireRain::UBTTask_FireRain()
+{
+	NodeName = "Fire Rain";
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_FireRain::ExecuteTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return EBTNodeResult::Failed;
+
+	ADragonBaseAI* Dragon = Cast<ADragonBaseAI>(AI->GetPawn());
+	if (!Dragon) return EBTNodeResult::Failed;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+	Dragon->EnableFlyingMode();
+
+	if (!AbilityComp || !AbilityClass)
+		return EBTNodeResult::Failed;
+
+	AActor* Player = UGameplayStatics::GetPlayerPawn(Dragon, 0);
+
+	UDragonAbility* Ability =
+		NewObject<UDragonAbility>(Dragon, AbilityClass);
+
+	AbilityComp->StartAbility(Ability, Player);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_FireRain::TickTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory,
+	float DeltaSeconds)
+{
+	AAIController* AI = OwnerComp.GetAIOwner();
+	if (!AI) return;
+
+	ADragonBaseAI* Dragon = Cast<ADragonBaseAI>(AI->GetPawn());
+	if (!Dragon) return;
+
+	UDragonAbilityComponent* AbilityComp =
+		Dragon->FindComponentByClass<UDragonAbilityComponent>();
+
+	if (!AbilityComp->IsAbilityActive())
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
---
+++ Source/FlightSystem/Public/AI/Task/BTTask_FireRain.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FireRain.generated.h"
+
+class UDragonAbility;
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTSYSTEM_API UBTTask_FireRain : public UBTTaskNode
+{
+	GENERATED_BODY()
+	UBTTask_FireRain();
+
+	virtual EBTNodeResult::Type ExecuteTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void TickTask(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UDragonAbility> AbilityClass;
+};
